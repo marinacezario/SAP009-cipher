02041846767f9d607e76642a421a386a0e42b92e
marinacezario <marina.cezario@gmail.com>
2023-01-23 13:20:07 -0300

    CSSeinicioJS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
+document.getElementById("botaoDecodificar").addEventListener("click", )
+document.getElementById("botaoCodificar").addEventListener("click", inserirDados)
+
+function inserirDados(){
+    if(document.getElementById("remetente").value != "" &&
+    document.getElementById("destinatario").value != "" &&
+    document.getElementById("mensagemCodificar").value != ""){
+        alert("Prontinho! Você receberá as novidades por e-mail!")
+      }else{
+        alert("Por favor, preencha os campos nome, e-mail e telefone!")
+      }
+}
+
 console.log(cipher);
+
+//chamar botão codificar
+//chamar botão decodificar
+//adicionar evento botao codificar
+//adicionar evento botao decodificar
