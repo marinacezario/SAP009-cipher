ddadf57b1183fa08f62baf55c82df46d6c318ff2
marinacezario <marina.cezario@gmail.com>
2023-01-27 12:13:42 -0300

    formatacao

--- src/index.js
+++ src/index.js
@@ -14,12 +14,21 @@ function inserirDados(){
     if(document.getElementById("remetente").value != "" &&
     document.getElementById("destinatario").value != "" &&
     document.getElementById("mensagemCodificar").value != ""){
-        alert("")
+        //mensagem codificada
     }else{
-        alert("")
+        alert(mensagemErro)
     }
 }
 
+window.scrollBy(0, window.innerHeight)
+if (offset,string == null && offset,string == undefined && offset,string == 0){
+  shownResult.classList.add('error');
+  return shownResult.innerHTML = mensageError;
+}else {
+shownResult.classList.remove('error')
+shownResult.classList.add('result')
+return shownResult.innerHTML = resultCipher;
+}
 console.log(cipher);
 
 //chamar botão codificar
