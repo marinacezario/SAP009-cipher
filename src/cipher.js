6c609ab6e52980f7cacdaddbf145e74a4916191d
marinacezario <marina.cezario@gmail.com>
2023-01-31 17:34:41 -0300

    JSeCipher

--- src/cipher.js
+++ src/cipher.js
@@ -2,26 +2,33 @@
 const cipher = {
 //função codificar
   encode: function (offset, string){
-    //colocar erro
-    if(offset !== Number || string !== []){
+    //colocar erro  
+    if(offset !== Number || string !== [] || offset === 0 || string === 0){
       throw new TypeError();
     }
-    let resultadoEncode = ""; 
+    const resultadoEncode = ""; 
     //fazer com que a função analise letra por letra da mensagem com o for
     for (let i = 0; i < string.lenght; i++) {
-          //buscar o numero na posição de cada letra 
+      //buscar o numero na posição de cada letra 
       const letraEncode = string.charCodeAt(i);
       if (letraEncode >= 65 && letraEncode <=90) {
+        // eslint-disable-next-line no-undef
         encode = ((letraEncode - 65 + offset) % 26) + 65;
 
+        // eslint-disable-next-line no-undef
         result = resultadoEncode + String.fromCharCode(encode);
       }//else if(letraEncode <= 64 || letraEncode >= 91){
       //}
     }
     //console.log(resultadoEncode);
-    return resultadoEncode,
+    return resultadoEncode;
   }
 }
+
+
+
+export default cipher;
+
 //(codigoDaLetra + desloc) % tamDoAlfabeto =>
 //((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra => 
 //"A".charCodeAt(0)
@@ -30,14 +37,13 @@ const cipher = {
 
 
 //} else if (letter >= 97 && letter <= 122) {
- // const calcOffsetLower = ((letter - 97 + offset) % 26) + 97;
-  //const offsetFinal = String.fromCharCode(calcOffsetLower);
+// const calcOffsetLower = ((letter - 97 + offset) % 26) + 97;
+//const offsetFinal = String.fromCharCode(calcOffsetLower);
 // results.push(offsetFinal);
 
 //} else {
 //const othersLetters = String.fromCharCode(letter);
- // results.push(othersLetters);
+// results.push(othersLetters);
 //}
 
 //return results.join("");
-export default cipher;
